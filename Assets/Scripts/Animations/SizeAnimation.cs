1371118bcaf19ae1872ebee73a72e5060b313670
Riazanov Igor <rebzya33@mail.ru>
2023-03-16 15:32:42 +0800

    Created TweenData

--- Assets/Scripts/Animations/SizeAnimation.cs
+++ Assets/Scripts/Animations/SizeAnimation.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,40 +6,18 @@ using UnityEngine.UI;
 
 public class SizeAnimation : MonoBehaviour
 {
-    [SerializeField] private Vector3 _scaleFrom;
-    [SerializeField] private Vector3 _scaleTo;
-    [SerializeField] [Min(0.0f)] private float _duration;
-    [SerializeField] AnimationCurve _scaleCurve;
+    [SerializeField] private Vector3TweenData _tweenData;
 
     private void Start() 
     {
-        StartCoroutine(PlayLoopedScalingAnimation(transform, _scaleFrom, _scaleTo, _duration, _scaleCurve));
+        ApplyAnimation(_tweenData);
     }
 
-    private IEnumerator PlayLoopedScalingAnimation(Transform transformToAnimate, Vector3 from, Vector3 to, float duration, AnimationCurve scaleCurve)
+    private void ApplyAnimation(Vector3TweenData tweenData)
     {
-        while(true)
-        {
-            yield return StartCoroutine(PlayScalingAnimation(transformToAnimate, to, duration, scaleCurve));
-            yield return StartCoroutine(PlayScalingAnimation(transformToAnimate, from, duration, scaleCurve));
-        }
-    }
-
-    private IEnumerator PlayScalingAnimation(Transform transformToAnimate, Vector3 to, float duration, AnimationCurve scaleCurve)
-    {
-        float enteredTime = Time.time;
-        Vector3 enteredScale = transformToAnimate.localScale;
-
-        while(Time.time < enteredTime + duration)
-        {
-            float elapsedTimePercent = (Time.time - enteredTime) / duration ;
-
-            Vector3 difference = to - enteredScale;
-            Vector3 scaleDifference = scaleCurve.Evaluate(elapsedTimePercent) * difference;
-
-            transform.localScale = enteredScale + scaleDifference;
-
-            yield return null;
-        }
+        transform
+            .DOScale(tweenData.EndValue, tweenData.Duration)
+            .SetEase(tweenData.Ease)
+            .SetLoops(-1, LoopType.Yoyo);
     }
 }
---
+++ Assets/Scripts/TweenStructures/TweenData.cs
@@ -0,0 +1,11 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TweenData <T>
+{
+    public T EndValue;
+    public float Duration;
+    public Ease Ease;
+}
