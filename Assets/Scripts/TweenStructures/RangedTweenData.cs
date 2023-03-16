f65880fa317fd575825ee58ee98b433972d47c08
Riazanov Igor <rebzya33@mail.ru>
2023-03-16 16:11:31 +0800

    Created SettingsButtomDropdownAnimation

--- Assets/Scripts/TweenStructures/RangedTweenData.cs
+++ Assets/Scripts/TweenStructures/RangedTweenData.cs
@@ -3,8 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class Vector3RangedRangedTweenData : RangedTweenData<Vector3> {}
 
+[System.Serializable]
 public class Vector2RangedRangedTweenData : RangedTweenData<Vector2> {}
 
 public class RangedTweenData <T> : TweenData<T>
