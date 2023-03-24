c3353d09c93862887815fbb2776cb38e44fa812f
Riazanov Igor <rebzya33@mail.ru>
2023-03-24 18:18:25 +0800

    Added PathFollowing to the Player

--- Assets/Scripts/PlayerComponents/Player.cs
+++ Assets/Scripts/PlayerComponents/Player.cs
@@ -7,17 +7,28 @@ public class Player : MonoBehaviour
     [SerializeField] private Character _character;
     [SerializeField] private ShootingPreferencesSo _shootingPreferences;
 
+    [SerializeField] private Path _path;
+    [SerializeField] private MovePreferencesSo _movePreferences;
+
     private FireRate _fireRate;
     private Weapon _weapon;
+    private PathFollowing _pathFollowing;
 
     private void Start() 
     {
         _weapon = _shootingPreferences.CreateWeapon(_character.ShootPoint);
         _fireRate = _shootingPreferences.CreateFireRate();
+        _pathFollowing = new PathFollowing(_path, this, _movePreferences);
     }
 
     public void Shoot()
     {
         _fireRate.Shoot(_weapon);
+    }
+
+    [ContextMenu("Move")]
+    public void Move()
+    {
+        _pathFollowing.MoveToNext();
     }
 }
