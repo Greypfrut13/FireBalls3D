f54774928d589f40040419498f5017041b0e0823
Riazanov Igor <rebzya33@mail.ru>
2023-03-24 18:06:00 +0800

    Created PathFollowing

---
+++ Assets/Scripts/Paths/PathFollowing.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class PathFollowing
+{
+    private readonly Path _path;
+    private readonly MonoBehaviour _follower;
+    private readonly MovePreferencesSo _preferences;
+
+    private int _pathSegmentIndex;
+
+    public PathFollowing(Path path, MonoBehaviour follower, MovePreferencesSo preferences)
+    {
+        _path = path;
+        _follower = follower;
+        _preferences = preferences;
+    }
+
+    public void MoveToNext()
+    {
+        if(_pathSegmentIndex >= _path.Segments.Count)
+            return;
+
+        PathSegment segment = _path.Segments[_pathSegmentIndex];
+        Transform[] waypoints = segment.Waypoints;
+
+        _follower.StartCoroutine(MoveBetwen(waypoints));
+    }
+
+    private IEnumerator MoveBetwen(IReadOnlyList<Transform> waypoints)
+    {
+        int index = 1;
+        Transform followerTransform = _follower.transform;
+
+        while(index < waypoints.Count)
+        {
+            Vector3 position = waypoints[index].position;
+
+            followerTransform
+                .DOLookAt(position, _preferences.RotateDuration)
+                .OnComplete(() => 
+                    followerTransform
+                        .DOMove(position, _preferences.DurationPerWaypoint));
+            
+            yield return new WaitForSeconds(_preferences.TotalDuration);
+
+            index++;
+        }
+
+        _pathSegmentIndex++;
+    }
+}
