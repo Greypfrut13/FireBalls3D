7323d4e7714818a7d77c230b9fa4a6d0d398154e
Riazanov Igor <rebzya33@mail.ru>
2023-05-09 12:26:17 +0800

    Created Menu Levels

---
+++ Assets/Scripts/Menu/Levels/MenuLevel.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MenuLevel : MonoBehaviour
+{
+    [SerializeField] private Transform _markerHolder;
+    [SerializeField] private TextMeshPro _number;
+
+    public Transform MarkerHolder => _markerHolder;
+
+    public void PaintNumber(Color color)
+    {
+        _number.color = color;
+    }
+}
